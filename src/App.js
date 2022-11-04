adfc62964e809143c6c0cd539cbeb1d47a39e32a
elysdominguez <elysdominguez2@gmail.com>
2022-11-04 15:45:09 +0100

    creating component and page folder with some routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import SideBar from "./components/SideBar";
+import Homepage from "./pages/Homepage";
 
 function App() {
-  return <div className="App">Hello there</div>;
+  return (
+    <div className="App">
+      <NavBar />
+      <SideBar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,3 @@
+export default function NavBar() {
+  return <div>Hello from NavBar</div>;
+}
