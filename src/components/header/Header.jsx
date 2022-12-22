8b7e40622b8ee97509921dace139c9e7ebe9fb2b
elysdominguez <elysdominguez2@gmail.com>
2022-12-22 16:19:11 +0100

    Fix style in CSS and add animation in name

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,16 +8,26 @@ export default function Header() {
   return (
     <header>
       <div className="container header_container">
-        <h5>Hello I'm</h5>
-        <h1>Eliana Dominguez</h1>
-        <h5 className="text-light">Fullstack Developer</h5>
-        <CTA />
-        <HeaderSocial />
-
-        <div className="me">
-          <img src={ME} alt="me" />
+        <div className="header_title">
+          <h5>Hi! I'm</h5>
+          <div className="name">
+            <span>E</span>
+            <span>L</span>
+            <span>I</span>
+            <span>A</span>
+            <span>N</span>
+            <span>A</span>
+          </div>
+          <h5 className="subtitle text-light">Fullstack Developer</h5>
         </div>
+        <div className="header_display">
+          <div className="me">
+            <img src={ME} alt="me" />
+          </div>
 
+          <CTA />
+          <HeaderSocial />
+        </div>
         <a href="#contact" className="scroll_down">
           Scroll Down
         </a>
