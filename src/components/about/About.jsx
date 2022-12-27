ce943cc99c35246077739ac42e08c88539be3824
elysdominguez <elysdominguez2@gmail.com>
2022-12-27 16:04:31 +0100

    Organized the page and started with About page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,17 @@ import "./about.css";
 export default function About() {
   return (
     <section id="about">
-      <p>About</p>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_me_image">
+            <img src="" alt="" />
+          </div>
+        </div>
+        <div className="about_content"></div>
+      </div>
     </section>
   );
 }
