fc67add6f6490869f41fe67c06bd0bff0fa7a4f5
elysdominguez <elysdominguez2@gmail.com>
2023-01-06 16:42:49 +0100

    Finished the about section and modified some css in header

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,18 +1,26 @@
 import "./about.css";
+import MeMovie from "../../images/MeMovieAbout.gif";
 
 export default function About() {
   return (
-    <section id="about">
+    <section className="about" id="about">
       <h5>Get To Know</h5>
       <h2>About Me</h2>
 
       <div className="container about_container">
         <div className="about_me">
           <div className="about_me_image">
-            <img src="" alt="" />
+            <img src={MeMovie} alt="a short video of myself" />
           </div>
         </div>
-        <div className="about_content"></div>
+        <div className="about_content">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem
+            voluptatem, tempora obcaecati a aperiam sint aspernatur libero sit
+            tenetur quae hic! Vel eius illo soluta delectus cum. Voluptatem,
+            neque asperiores?
+          </p>
+        </div>
       </div>
     </section>
   );
