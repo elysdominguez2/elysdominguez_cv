ba52246b9bf202746e71c63306156c2a3311737f
elysdominguez <elysdominguez2@gmail.com>
2023-01-07 15:21:08 +0100

    Started with the Experience section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,5 +1,33 @@
 import "./experience.css";
+import { BsFillPatchCheckFill } from "react-icons/bs";
 
 export default function Experience() {
-  return <section id="experience">Experience</section>;
+  return (
+    <section id="experience">
+      <h5>What skills I have</h5>
+      <h2>My experience</h2>
+
+      <div className="container experience_container">
+        <div className="experience_hard_skills">
+          <h3>Skills</h3>
+          <div className="skills_content">
+            <article className="skills_details">
+              <BsFillPatchCheckFill />
+              <h4>HTML</h4>
+            </article>
+          </div>
+        </div>
+
+        <div className="experience_soft_skills">
+          <h3>Soft Skills</h3>
+          <div className="skills_content">
+            <article className="skills_details">
+              <BsFillPatchCheckFill />
+              <h4>HTML</h4>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
